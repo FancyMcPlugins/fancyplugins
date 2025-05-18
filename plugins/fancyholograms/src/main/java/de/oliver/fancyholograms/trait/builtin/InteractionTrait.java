384a73aaf5a1cf8a6d6e7e411ccfc3e50acf007a
Oliver <oschi1303@gmail.com>
2025-05-18 12:56:47 +0200

    Calculate height and width

--- plugins/fancyholograms/src/main/java/de/oliver/fancyholograms/trait/builtin/InteractionTrait.java
+++ plugins/fancyholograms/src/main/java/de/oliver/fancyholograms/trait/builtin/InteractionTrait.java
@@ -1,5 +1,6 @@
 package de.oliver.fancyholograms.trait.builtin;
 
+import de.oliver.fancyholograms.api.data.TextHologramData;
 import de.oliver.fancyholograms.api.trait.HologramTrait;
 import de.oliver.fancyholograms.api.trait.HologramTraitClass;
 import de.oliver.fancyholograms.util.PluginUtils;
@@ -78,6 +79,7 @@ public class InteractionTrait extends HologramTrait {
             ));
             this.hitbox.setSaveToFile(false);
             this.hitbox.getData().setType(EntityType.INTERACTION);
+            this.hitbox.getData().setDisplayName("<empty>");
 
             // TODO add actions to hitbox
 
@@ -89,12 +91,33 @@ public class InteractionTrait extends HologramTrait {
         this.hitbox.getData().setLocation(hologram.getData().getLocation());
 
         NpcAttribute widthAttr = FancyNpcsPlugin.get().getAttributeManager().getAttributeByName(EntityType.INTERACTION, "width");
-        this.hitbox.getData().addAttribute(widthAttr, "2.0"); // TODO calculate based on hologram text, scale and billboard
+        this.hitbox.getData().addAttribute(widthAttr, String.valueOf(calcWidth()));
 
         NpcAttribute heightAttr = FancyNpcsPlugin.get().getAttributeManager().getAttributeByName(EntityType.INTERACTION, "height");
-        this.hitbox.getData().addAttribute(heightAttr, "2.0"); // TODO calculate based on hologram text, scale and billboard
+        this.hitbox.getData().addAttribute(heightAttr, String.valueOf(calcHeight()));
 
         this.hitbox.updateForAll();
+    }
+
+    private double calcHeight() {
+        int lines = ((TextHologramData) hologram.getData()).getText().size();
+        double heightPerLine = 0.25;
+        double scale = ((TextHologramData) hologram.getData()).getScale().y;
+        return lines * heightPerLine * scale;
+    }
+
+    private double calcWidth() {
+        List<String> lines = ((TextHologramData) hologram.getData()).getText();
+        double maxCharacters = 0;
+        for (String line : lines) {
+            if (line.length() > maxCharacters) {
+                maxCharacters = line.length();
+            }
+        }
+
+        double widthPerCharacter = 0.12;
+        double scale = ((TextHologramData) hologram.getData()).getScale().x;
+        return maxCharacters * widthPerCharacter * scale;
     }
 
     public enum Action {
